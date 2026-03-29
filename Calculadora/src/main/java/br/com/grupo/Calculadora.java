37f23c209f492be5b9e93b38e4e98860cb97be9f
Gustavo M. Scot <gustavom.scot@gmail.com>
2026-03-29 18:15:35 -0300

    feat: cria interface de console para calculadora

--- Calculadora/src/main/java/br/com/grupo/Calculadora.java
+++ Calculadora/src/main/java/br/com/grupo/Calculadora.java
@@ -1,4 +1,4 @@
-package main.java.br.com.grupo;
+package br.com.grupo;
 
 public class Calculadora {
 
--- Calculadora/src/main/java/br/com/grupo/Main.java
+++ Calculadora/src/main/java/br/com/grupo/Main.java
@@ -1,17 +1,62 @@
 package br.com.grupo;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        Calculadora calculadora = new Calculadora();
+
+        int opcao;
+
+        do {
+            System.out.println("\n=== CALCULADORA ===");
+            System.out.println("1 - Somar");
+            System.out.println("2 - Subtrair");
+            System.out.println("3 - Multiplicar");
+            System.out.println("4 - Dividir");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+
+            opcao = scanner.nextInt();
+
+            if (opcao >= 1 && opcao <= 4) {
+                System.out.print("Digite o primeiro número: ");
+                double a = scanner.nextDouble();
+
+                System.out.print("Digite o segundo número: ");
+                double b = scanner.nextDouble();
+
+                double resultado = 0;
+
+                try {
+                    switch (opcao) {
+                        case 1:
+                            resultado = calculadora.somar(a, b);
+                            break;
+                        case 2:
+                            resultado = calculadora.subtrair(a, b);
+                            break;
+                        case 3:
+                            resultado = calculadora.multiplicar(a, b);
+                            break;
+                        case 4:
+                            resultado = calculadora.dividir(a, b);
+                            break;
+                    }
+
+                    System.out.println("Resultado: " + resultado);
+
+                } catch (ArithmeticException e) {
+                    System.out.println("Erro: " + e.getMessage());
+                }
+            }
+
+        } while (opcao != 0);
+
+        System.out.println("Encerrando...");
+        scanner.close();
     }
 }
